8b594152866da34d3dc078378e499f526f8778d2
Matthew Polizzotti <mpolizzotti@unicon.net>
2017-02-01 20:34:15 -0700

    Add font-awesome and bootstrap fonts to different folders.

--- tasks/copy/fonts.js
+++ tasks/copy/fonts.js
@@ -3,15 +3,28 @@ let gulp = require('gulp'),
     config = require('../../config'),
     path = require('path');
 
+// Helper.
+function copyFontsHelper(source, destination) {
+    return gulp
+        .src([source])
+        .pipe(gulp.dest(destination));
+}
+
 // Copy libraries.
 gulp.task('copy:fonts', () => {
     'use strict';
 
-    let bootstrapFonts = path.join(config.development.paths.root, 'node_modules/bootstrap/fonts/**/*');
-    let fontAwesomeFonts = path.join(config.development.paths.root, 'node_modules/font-awesome/fonts/**/*');
-    let fontsPath = config.development.paths.fonts;
+    let bootstrapFontSourcePath = path.join(config.development.paths.root, 'node_modules/bootstrap/fonts/**/*');
+    let fontAwesomeFontSourcePath = path.join(config.development.paths.root, 'node_modules/font-awesome/fonts/**/*');
+    let bootstrapFontDestPath = path.join(config.development.paths.fonts, 'bootstrap/');
+    let fontAwesomeFontDestPath = path.join(config.development.paths.fonts, 'font-awesome/');
 
-    return gulp
-        .src([bootstrapFonts, fontAwesomeFonts])
-        .pipe(gulp.dest(fontsPath));
+    let fonts = [
+        {source: bootstrapFontSourcePath, dest: bootstrapFontDestPath},
+        {source: fontAwesomeFontSourcePath, dest: fontAwesomeFontDestPath},
+    ];
+
+    for (let i = 0; i < fonts.length; i++) {
+        copyFontsHelper(fonts[i].source, fonts[i].dest);
+    }
 });
