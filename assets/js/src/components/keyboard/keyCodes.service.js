c76ebdbd3966665158ad40af9151a279055b32b9
Matthew Polizzotti <mpolizzotti@unicon.net>
2017-03-14 07:49:43 -0700

    Apply tabbing restriction to side navigation.

---
+++ assets/js/src/components/keyboard/keyCodes.service.js
@@ -0,0 +1,26 @@
+/**
+ * Service houses a common set of utilities for managing keyboard events and their keycodes.
+ */
+
+export const BACKSPACE = 8;
+export const TAB = 9;
+
+export const NUM_ZERO = 48;
+export const NUM_ONE = 49;
+export const NUM_TWO = 50;
+export const NUM_THREE = 51;
+export const NUM_FOUR = 52;
+export const NUM_FIVE = 53;
+export const NUM_SIX = 54;
+export const NUM_SEVEN = 55;
+export const NUM_EIGHT = 56;
+export const NUM_NINE = 57;
+
+/**
+ * Determines if a keycode belongs to a number key.
+ * @param keyCode {number} the code of the key pressed, generally pulled from event.which
+ * @returns {boolean} whether or not it's a number key
+ */
+export function isNumberKey(keyCode) {
+    return keyCode >= NUM_ZERO && keyCode <= NUM_NINE;
+}
--- assets/js/src/components/keyboard/restrict.tabbing.service.js
+++ assets/js/src/components/keyboard/restrict.tabbing.service.js
@@ -1,12 +1,15 @@
+import * as keyCodes from '../keyboard/keyCodes.service';
+
 const MODULE_NAME = 'solitude.restrict.tabbing.service';
 
 /**
-* The RestrictTabbingService class manages...
+* The RestrictTabbingService class restricts keyboard tabbing, when enabled,
+* to the rootNode passed into the RestrictTabbingService class.
 *
 * @class
 * @type Class
 * @return public API
- */
+*/
 export class RestrictTabbingService {
     constructor(rootNode) {
         this.rootNode = rootNode;
@@ -33,12 +36,17 @@ export class RestrictTabbingService {
         }
 
         this.selectors = this.rootNode.querySelectorAll(':scope a, :scope button');
+        this.selectors = _.map(this.selectors, (node) => {
+            node.setAttribute('data-tabbing-id', _.uniqueId());
+            return node;
+        });
     }
 
     prepareDOM() {
         if (!this.checkNodes()) {
             return;
         }
+
 
         this.selectors[0].focus();
     }
@@ -59,6 +67,44 @@ export class RestrictTabbingService {
         this.rootNode.removeEventListener('keydown', this.keyDownListener, false);
     }
 
+    calculateKeyDownFocus(e) {
+        if (!this.checkNodes()) {
+            return;
+        }
+
+        if (!e) {
+            console.info('RestrictTabbingService. Missing event.');
+            return;
+        }
+
+        let firstSelector = this.selectors[0],
+            firstSelectorId = firstSelector.getAttribute('data-tabbing-id'),
+            activeElement = document.activeElement,
+            activeElementId = activeElement.getAttribute('data-tabbing-id'),
+            shiftKey = e.shiftKey,
+            position = 0;
+
+        for (let i = 0; i < this.selectors.length; i++) {
+            let selectorId = this.selectors[i].getAttribute('data-tabbing-id');
+
+            if (activeElementId === selectorId) {
+                if (!shiftKey) {
+                    position = (i + 1);
+                    if (position >= this.selectors.length) {
+                        position = 0;
+                    }
+                } else {
+                    position = (i - 1);
+                    if (position < 0) {
+                        position = (this.selectors.length - 1);
+                    }
+                }
+
+                this.selectors[position].focus();
+            }
+        }
+    }
+
     keyDownListener(e) {
         if (!this.checkNodes()) {
             return;
@@ -69,6 +115,10 @@ export class RestrictTabbingService {
             return;
         }
 
-        console.log('Keydown: ', e);
+        if (e.keyCode === keyCodes.TAB) {
+            e.preventDefault();
+            e.stopImmediatePropagation();
+            this.calculateKeyDownFocus(e)
+        }
     }
 }
