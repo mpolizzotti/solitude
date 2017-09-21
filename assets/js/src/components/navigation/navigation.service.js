65603dde9e26b0b6e7d00251e704d3d74e5b4ae6
Matthew Polizzotti <mpolizzotti@unicon.net>
2017-09-21 16:30:21 -0700

    Bug fix. Prevent iOS background scrolling.

--- assets/js/src/components/navigation/navigation.service.js
+++ assets/js/src/components/navigation/navigation.service.js
@@ -44,6 +44,14 @@ export class NavigationService {
         this.restrictTabbingService = new RestrictTabbingService(this.navigationSidebar);
     }
 
+    onTouchMove(e) {
+        if (!this.checkNodes()) {
+            return;
+        }
+
+        e.preventDefault();
+    }
+
     toggleNavigationMenu(e) {
         if (!this.checkNodes()) {
             return;
@@ -54,12 +62,14 @@ export class NavigationService {
             this.siteWrapper.classList.remove('navigation-open');
             this.siteWrapper.classList.add('navigation-close');
             this.body.classList.remove('no-scroll');
+            this.body.addEventListener('touchmove', this.onTouchMove, false);
             this.restrictTabbingService.disable();
             this.menuButton.focus();
         } else {
             this.siteWrapper.classList.remove('navigation-close');
             this.siteWrapper.classList.add('navigation-open');
             this.body.classList.add('no-scroll');
+            this.body.removeEventListener('touchmove', this.onTouchMove, false);
             this.restrictTabbingService.enable();
             this.navigationClose.focus();
         }
